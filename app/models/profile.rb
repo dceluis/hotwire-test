caab13a5da97f3a78be388c3f943314efc5f1d6e
Luis Sanchez <dceluis@outlook.com>
2022-09-03 20:42:34 -0500

    Validate length

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,6 +4,9 @@ class Profile < ApplicationRecord
   validates_format_of :email, with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
   validates_format_of :phone, with: /\A[0-9]{3}-[0-9]{3}-[0-9]{4}\z/
 
+  validates_length_of :first_name, maximum: 25
+  validates_length_of :last_name, maximum: 50
+
   has_many :employments, dependent: :destroy
 
   def full_name
